1de55a4888c4b6b7d5cde4ca279c2c58c40efed0
Admin <reteamboss@gmail.com>
2024-08-29 17:11:19 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    var strOne = "ПриВет, кАк деЛа"
+    var count = 0
+    for (i in strOne){
+        if (i.isUpperCase()){
+            println(i)
+            count+=1
+        }
+
+    }
+    println("Больших символов $count")
 }
